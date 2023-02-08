5555627bd8d101861752f04aa4d2706388e7b208
Erin McDonald <tug94529@temple.edu>
2023-02-08 16:14:44 -0500

    remove extra styles

--- components/ButtonsComponent.tsx
+++ components/ButtonsComponent.tsx
@@ -36,67 +36,10 @@ function ButtonsComponent({
 export default ButtonsComponent;
 
 const styles = StyleSheet.create({
-  exerciseName: {
-    fontWeight: "bold",
-  },
-  columnHeadersContainer: {
-    flexDirection: "row",
-
-    width: 350,
-  },
-  headerText: {
-    fontWeight: "bold",
-  },
-  inputContainer: {
-    flexDirection: "row",
-    height: 30,
-    width: 350,
-    backgroundColor: "white",
-  },
-  detailsContainer: {
-    backgroundColor: "#e0e0e0",
-    margin: 4,
-    borderRadius: 8,
-    alignContent: "center",
-    justifyContent: "center",
-    textAlign: "center",
-  },
-  detailsText: {
-    textAlign: "center",
-  },
-  addSetButton: {
-    flex: 1,
-    backgroundColor: "#e0e0e0",
-    borderRadius: 8,
-    height: 30,
-    marginTop: 10,
-    marginBottom: 20,
-    justifyContent: "center",
-  },
-  addSetButtonText: {
-    textAlign: "center",
-  },
-  deleteContainer: {
-    backgroundColor: "#ff7885",
-    flex: 1,
-  },
-  deleteText: {
-    color: "white",
-    alignSelf: "flex-end",
-    fontSize: 18,
-  },
   buttonPressed: {
     opacity: 0.5,
   },
-  deleteExerciseButton: {},
-  exerciseNameContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: 10,
-  },
-  deleteExerciseText: {
-    color: "red",
-  },
+
   addExerciseButton: {
     backgroundColor: "gray",
     padding: 10,
@@ -115,9 +58,5 @@ const styles = StyleSheet.create({
   },
   cancelWorkoutText: {
     textAlign: "center",
-  },
-  contentContainer: {
-    height: 400,
-    alignItems: "center",
   },
 });
